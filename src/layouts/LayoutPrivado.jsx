0ed5ecef17d0d154bda7cf8c4b8e7a853c18be77
AndriuuU <andresitomolinaclan@gmail.com>
2024-12-08 22:19:39 +0100

    update

--- src/layouts/LayoutPrivado.jsx
+++ src/layouts/LayoutPrivado.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import PrivateNavbar from "../components/PrivateNavbar";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 
 const LayoutPrivado = () => {
   const { user } = useContext(UserContext);
