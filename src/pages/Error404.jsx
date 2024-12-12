2229bc6f7f09122527ceed204248a875d8ff864a
AndriuuU <andresitomolinaclan@gmail.com>
2024-12-12 19:58:11 +0100

    Animacion error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
-import '../style/main.scss';  // Asegúrate de importar el archivo de CSS
-import coche from '../../public/img/cocheVolcado.png';
+import React from 'react'
+import '../style/main.scss'  // Asegúrate de importar el archivo de CSS
+import coche from '../../public/img/cocheVolcado.png'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 const Error404 = () => {
   return (
-    <section className="error-404">
-      <h1 className="error-title">
-        {['E', 'r', 'r', 'o', 'r', ' ', '4', '0', '4'].map((letter, index) => (
-          <span key={index} className="error-letter">
-            {letter}
-          </span>
-        ))}
-      </h1>
-      <p className="error-message">
-        Parece que te has perdido, pero no te preocupes, vuelve al inicio y todo estará bien.
-      </p>
-      <img
-        src={coche} // Reemplaza con tu imagen de coche
-        alt="Coche"
-        className="error-image"
-      />
-      <a href="/" className="error-button">Volver al inicio</a>
-    </section>
-  );
-};
+    <div>
+      <Navbar /> {/* Asegúrate de que el navbar esté aquí */}
+      
+      <section className="error-404">
+        <h1 className="error-title">
+          {['E', 'r', 'r', 'o', 'r', ' ', '4', '0', '4'].map((letter, index) => (
+            <span key={index} className="error-letter">
+              {letter}
+            </span>
+          ))}
+        </h1>
+        <p className="error-message">
+          Parece que te has perdido, pero no te preocupes, vuelve al inicio y todo estará bien.
+        </p>
+        <img src={coche} alt="Coche" className="error-image" />
+        <a href="/" className="error-button">Volver al inicio</a>
+      </section>
 
-export default Error404;
+      <Footer /> {/* Asegúrate de que el footer esté aquí */}
+    </div>
+  )
+}
+
+export default Error404
